165887908d3f981df68a5c06ada8d061fdd497fb
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2022-03-18 21:10:49 +0800

    Update sub.js

--- sub.js
+++ sub.js
@@ -5,7 +5,7 @@ async function handleRequest(request) {
   //Generate Radom Value
   function randomString(e) {    
     e = e || 32;
-    var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678",
+    var t = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
     a = t.length,
     n = "";
     for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
